01ef787fd6fe2d56ee8285070a94aa5289531569
juanzuloaga <jzuloaga65@gmail.com>
2024-05-16 16:08:33 -0600

    Adición de autores

--- to_run_Cargar.R
+++ to_run_Cargar.R
@@ -1,6 +1,6 @@
 
 
-setwd("C:/Talleres_Ecodiversa_R/")
+setwd("C:/Talleres_R/")
 rmarkdown::render(input = "./IntroR_Cargar/IntroR_Cargar.Rmd",
                   output_file =  "Intro_Cargar.html",
                   output_dir = "./IntroR_Cargar")
